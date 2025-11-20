f5bda5c140976dcbdec2570ba9f3c938c17750e3
ryanw18 <ryanw18@colostate.edu>
2025-11-20 13:37:48 -0700

    final changes

--- ReadingApp/appsetup/urls.py
+++ ReadingApp/appsetup/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('api/log-trial/', views.log_trial, name='log_trial'),
+    path('api/save-survey/', views.save_survey, name='save_survey'),
 ]
